4dc2c406d12024fca51c95d2e76d4c7d205b7251
Angel Misevski <amisevsk@redhat.com>
2020-10-29 19:42:35 -0400

    temp work; drop

--- pkg/apis/workspaces/v1alpha1/devworkspace_conversion.go
+++ pkg/apis/workspaces/v1alpha1/devworkspace_conversion.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	"errors"
+	"github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
@@ -9,7 +10,16 @@ import (
 var _ conversion.Convertible = (*DevWorkspace)(nil)
 
 func (src *DevWorkspace) ConvertTo(dstRaw conversion.Hub) error {
-	return errors.New("Unimplemented")
+	dst := dstRaw.(*v1alpha2.DevWorkspace)
+
+	// Basic conversions of metadata and status
+	dst.ObjectMeta = src.ObjectMeta
+	dst.Status.WorkspaceId = src.Status.WorkspaceId
+	dst.Status.IdeUrl = src.Status.IdeUrl
+	dst.Status.Phase = v1alpha2.WorkspacePhase(src.Status.Phase)
+
+
+	return nil
 }
 
 func (dst *DevWorkspace) ConvertFrom(srcRaw conversion.Hub) error {
