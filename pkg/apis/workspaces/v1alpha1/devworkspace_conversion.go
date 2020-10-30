bd6bcd33deab60bbac4edc909b8796acf80f314d
Angel Misevski <amisevsk@redhat.com>
2020-10-29 20:52:13 -0400

    test implmenting generated method
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/apis/workspaces/v1alpha1/devworkspace_conversion.go
+++ pkg/apis/workspaces/v1alpha1/devworkspace_conversion.go
@@ -3,13 +3,14 @@ package v1alpha1
 import (
 	"errors"
 	"github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
-	"sigs.k8s.io/controller-runtime/pkg/conversion"
+	conv "sigs.k8s.io/controller-runtime/pkg/conversion"
+	apimachineryconv "k8s.io/apimachinery/pkg/conversion"
 )
 
 // Spokes for conversion have to satisfy the Convertible interface.
-var _ conversion.Convertible = (*DevWorkspace)(nil)
+var _ conv.Convertible = (*DevWorkspace)(nil)
 
-func (src *DevWorkspace) ConvertTo(dstRaw conversion.Hub) error {
+func (src *DevWorkspace) ConvertTo(dstRaw conv.Hub) error {
 	dst := dstRaw.(*v1alpha2.DevWorkspace)
 
 	// Basic conversions of metadata and status
@@ -22,6 +23,10 @@ func (src *DevWorkspace) ConvertTo(dstRaw conversion.Hub) error {
 	return nil
 }
 
-func (dst *DevWorkspace) ConvertFrom(srcRaw conversion.Hub) error {
+func (dst *DevWorkspace) ConvertFrom(srcRaw conv.Hub) error {
 	return errors.New("Unimplemented")
+}
+
+func autoConvert_v1alpha1_Component_To_v1alpha2_Component(in *Component, out *v1alpha2.Component, s apimachineryconv.Scope) error {
+	return nil
 }
