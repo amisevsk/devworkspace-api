f5138f26d4db821fbcaa6772d7be6f477270304a
Angel Misevski <amisevsk@redhat.com>
2020-10-29 20:47:48 -0400

    Attempt 1 at using conversion-gen
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/apis/workspaces/v1alpha1/doc.go
+++ pkg/apis/workspaces/v1alpha1/doc.go
@@ -1,4 +1,5 @@
 // Package v1alpha1 contains API Schema definitions for the org v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/devfile/api/pkg/apis/workspaces/v1alpha2
 // +groupName=workspace.devfile.io
 package v1alpha1
